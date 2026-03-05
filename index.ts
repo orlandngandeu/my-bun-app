1c8ea0320b1c0a784dc17547bf7ac03ca9e3282b
orlan17ngandeu <orlan.ngandeu@homesolutionsworld.org>
2026-03-05 01:07:38 +0100

    installation de biome

--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-console.log("Hello via Bun!");
+import { serve } from "bun";
+
+const server = serve({
+  port: 3000,
+  routes: {
+    "/": () => new Response('Bun!'),
+  }
+});
+
+console.log(`Listening on ${server.url}`);
+
