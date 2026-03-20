190c8cb9d0c5063692f34d4198b1e90b998133bb
orlan17ngandeu <orlan.ngandeu@homesolutionsworld.org>
2026-03-20 13:40:26 +0100

    prise en main de bun pour index.html

--- index.ts
+++ index.ts
@@ -1,11 +1,16 @@
 import { serve } from "bun";
+import figlet from "figlet";
+import index from "./index.html";
 
 const server = serve({
-  port: 3000,
-  routes: {
-    "/": () => new Response('Bun!'),
-  }
+	port: 3000,
+	routes: {
+		"/": index,
+		"/figlet": () => {
+			const body = figlet.textSync("ORLAN NGANDEU!");
+			return new Response(body);
+		},
+	},
 });
 
 console.log(`Listening on ${server.url}`);
-
